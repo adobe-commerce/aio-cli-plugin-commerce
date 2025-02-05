d8dfa54e280f099d00399f5da5a411c0ab094e2c
Stephen Rugh <rugh@adobe.com>
2025-02-05 13:40:19 -0600

    remove demo store from selection for now
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- src/utils/initialization.js
+++ src/utils/initialization.js
@@ -36,7 +36,7 @@ export async function initialization (args, flags) {
 
   // TEMPLATE SELECTION
   const template = await promptSelect('Which template would you like to use?', [
-    'adobe-commerce/adobe-demo-store',
+    // 'adobe-commerce/adobe-demo-store', // TODO: uncomment once demo store is a template repo.
     'hlxsites/aem-boilerplate-commerce',
     'AdobeDevXSC/citisignal-one'
   ])
