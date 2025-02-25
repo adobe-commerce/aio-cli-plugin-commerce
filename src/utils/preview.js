20584b82a77f3e1fc7b53cbf75033892c3e11d92
Stephen Rugh <rugh@adobe.com>
2025-02-25 12:45:33 -0600

    use existing fetchWithRetry function
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- src/utils/preview.js
+++ src/utils/preview.js
@@ -1,35 +1,7 @@
 import config from '@adobe/aio-lib-core-config'
 import Logger from '@adobe/aio-lib-core-logging'
+import { fetchWithRetry } from './fetchWithRetry'
 const aioLogger = Logger('commerce:preview.js')
-
-/**
- * TODO: extract to other util file and use everywhere we have some fetch + callback + retry needs
- * @param detailsUrl
- * @param fileCount
- * @param callback
- * @param interval
- * @param maxRetries
- */
-export async function waitForComplete (detailsUrl, callback, interval = 1000, maxRetries = 10) {
-  let retryCount = 0
-  while (retryCount <= maxRetries) {
-    const res = await fetch(detailsUrl)
-    if (res.ok) {
-      const data = await res.json()
-      aioLogger.debug(data)
-      const isComplete = await callback(data)
-      if (isComplete) {
-        return
-      }
-    } else {
-      aioLogger.debug(`details fetch failed, retrying (${retryCount + 1}/${maxRetries})`, res)
-    }
-
-    await new Promise(resolve => setTimeout(resolve, interval))
-    retryCount++
-  }
-  throw new Error('fetch failed after max retries')
-}
 
 // TODO: DRY - only diff for the preview/publish functions is the API url and log detail (use of term preview vs publish).
 // we should refactor
@@ -62,10 +34,10 @@ export async function previewContent (files) {
       const detailsUrl = `${resJson.links.self}/details`
       aioLogger.debug(detailsUrl)
       console.log('⏳ Started batch content preview job.')
-      await waitForComplete(detailsUrl, (data) => {
+      await fetchWithRetry(detailsUrl, {}, (data) => {
         return data.data.phase === 'completed' &&
-        data.progress.processed === previewFiles.length &&
-        data.progress.success === previewFiles.length
+          data.progress.processed === previewFiles.length &&
+          data.progress.success === previewFiles.length
       })
       console.log('✅ Batch content preview job completed successfully.')
     }
@@ -104,10 +76,10 @@ export async function publishContent (files) {
       const detailsUrl = `${resJson.links.self}/details`
       aioLogger.debug(detailsUrl)
       console.log('⏳ Started batch content publish job.')
-      await waitForComplete(detailsUrl, (data) => {
+      await fetchWithRetry(detailsUrl, {}, (data) => {
         return data.data.phase === 'completed' &&
-        data.progress.processed === publishFiles.length &&
-        data.progress.success === publishFiles.length
+          data.progress.processed === publishFiles.length &&
+          data.progress.success === publishFiles.length
       })
       console.log('✅ Batch content publish job completed successfully.')
     }
