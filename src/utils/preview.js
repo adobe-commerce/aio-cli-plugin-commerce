0f22ebda31fdcd4ce55218ea53c6a5abe35e6625
Stephen Rugh <rugh@adobe.com>
2025-02-25 09:41:22 -0600

    make waitForComplete more generic
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- src/utils/preview.js
+++ src/utils/preview.js
@@ -2,6 +2,34 @@ import config from '@adobe/aio-lib-core-config'
 import Logger from '@adobe/aio-lib-core-logging'
 const aioLogger = Logger('commerce:preview.js')
 
+/**
+ * TODO: extract to other util file and use everywhere we have some fetch + callback + retry needs
+ * @param detailsUrl
+ * @param fileCount
+ * @param callback
+ * @param interval
+ * @param maxRetries
+ */
+export async function waitForComplete (detailsUrl, callback, interval = 1000, maxRetries = 10) {
+  let retryCount = 0
+  while (retryCount <= maxRetries) {
+    const res = await fetch(detailsUrl)
+    if (res.ok) {
+      const data = await res.json()
+      aioLogger.debug(data)
+      const isComplete = await callback(data)
+      if (isComplete) {
+        return
+      }
+    } else {
+      aioLogger.debug(`details fetch failed, retrying (${retryCount + 1}/${maxRetries})`, res)
+    }
+
+    await new Promise(resolve => setTimeout(resolve, interval))
+    retryCount++
+  }
+}
+
 // TODO: DRY - only diff for the preview/publish functions is the API url and log detail (use of term preview vs publish).
 // we should refactor
 /**
@@ -33,7 +61,11 @@ export async function previewContent (files) {
       const detailsUrl = `${resJson.links.self}/details`
       aioLogger.debug(detailsUrl)
       console.log('✅ Started batch content preview job.')
-      await waitForComplete(detailsUrl, previewFiles.length)
+      await waitForComplete(detailsUrl, (data) => {
+        return data.data.phase === 'completed' &&
+        data.progress.processed === previewFiles.length &&
+        data.progress.success === previewFiles.length
+      })
       console.log('✅ Batch content preview job completed successfully.')
     }
   } catch (error) {
@@ -42,35 +74,6 @@ export async function previewContent (files) {
   }
 }
 
-/**
- *
- * @param detailsUrl
- * @param fileCount
- * @param interval
- * @param maxRetries
- */
-export async function waitForComplete (detailsUrl, fileCount, interval = 1000, maxRetries = 10) {
-  let retryCount = 0
-  while (retryCount <= maxRetries) {
-    const res = await fetch(detailsUrl)
-    if (res.ok) {
-      const data = await res.json()
-      const isComplete =
-        data.data.phase === 'completed' &&
-        data.progress.processed === fileCount &&
-        data.progress.success === fileCount
-      aioLogger.debug(data)
-      if (isComplete) {
-        return
-      }
-    } else {
-      aioLogger.debug(`details fetch failed, retrying (${retryCount + 1}/${maxRetries})`, res)
-    }
-
-    await new Promise(resolve => setTimeout(resolve, interval))
-    retryCount++
-  }
-}
 /**
  * For DA Live Preview, we must publish some files. This must be done AFTER preview.
  * https://www.aem.live/docs/admin.html#tag/publish/operation/bulkPublish
@@ -100,7 +103,11 @@ export async function publishContent (files) {
       const detailsUrl = `${resJson.links.self}/details`
       aioLogger.debug(detailsUrl)
       console.log('✅ Started batch content publish job.')
-      await waitForComplete(detailsUrl, publishFiles.length)
+      await waitForComplete(detailsUrl, (data) => {
+        return data.data.phase === 'completed' &&
+        data.progress.processed === publishFiles.length &&
+        data.progress.success === publishFiles.length
+      })
       console.log('✅ Batch content publish job completed successfully.')
     }
   } catch (error) {
