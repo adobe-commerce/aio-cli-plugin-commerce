e9fdf21cf24df565d31e8c8b00eda87e865bcbda
Stephen Rugh <rugh@adobe.com>
2025-02-25 08:28:59 -0600

    publish all files
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- src/utils/preview.js
+++ src/utils/preview.js
@@ -12,10 +12,10 @@ const aioLogger = Logger('commerce:preview.js')
 export async function previewContent (files) {
   const { org, repo } = config.get('commerce.github')
   if (!org || !repo) throw new Error('Missing Github Org and Repo')
+  const url = new URL(`https://admin.hlx.page/preview/${org}/${repo}/main/*`)
   try {
     const previewFiles = files.map(file => new URL(file).pathname)
     aioLogger.debug(previewFiles)
-    const url = new URL(`https://admin.hlx.page/preview/${org}/${repo}/main/*`)
     const res = await fetch(url, {
       method: 'POST',
       headers: {
@@ -37,32 +37,25 @@ export async function previewContent (files) {
   }
 }
 
-const filesToPublish = [
-  '/configs.json',
-  '/placeholders.json',
-  // TODO: nav, footer, and mini-cart are only necessary for base boilerplate, not citisignal. Add logic to conditionally determine whether we need to publish these at all. We may need to find if there are any citisignal files which need publishing too.
-  '/nav',
-  '/footer',
-  '/mini-cart'
-]
-
 /**
  * For DA Live Preview, we must publish some files. This must be done AFTER preview.
  * https://www.aem.live/docs/admin.html#tag/publish/operation/bulkPublish
+ * @param files
  */
-export async function publishContent () {
+export async function publishContent (files) {
   const { org, repo } = config.get('commerce.github')
   if (!org || !repo) throw new Error('Missing Github Org and Repo')
   const url = new URL(`https://admin.hlx.page/live/${org}/${repo}/main/*`)
-
   try {
+    const publishFiles = files.map(file => new URL(file).pathname)
+    aioLogger.debug(publishFiles)
     const res = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        paths: [...filesToPublish]
+        paths: [...publishFiles]
       })
     })
     if (res.status !== 202) {
